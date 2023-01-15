fa9caffa58627b00ef8c70a0efb4857e16970b1e
felixmerz <65616000+felixmerz00@users.noreply.github.com>
2023-01-15 21:04:45 +0100

    tests for HasBoatState

--- src/test/ch/uzh/board/position/PositionTest.java
+++ src/test/ch/uzh/board/position/PositionTest.java
@@ -3,6 +3,7 @@ package ch.uzh.board.position;
 import ch.uzh.board.Column;
 import ch.uzh.board.GridType;
 import ch.uzh.board.Row;
+import ch.uzh.boat.PatrolBoat;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
@@ -33,7 +34,7 @@ class PositionTest {
         assertFalse(testPosition.attack());
     }
 
-    // Test revealContent method when Position is in HasNoBoatState.
+    // Test revealContent before an attack method when Position is in HasNoBoatState.
     @Test
     void hasNoBoatRevealContentBeforeAttack() {
         Position testPosition = new Position(Column.A, Row._0);
@@ -42,6 +43,7 @@ class PositionTest {
         assertEquals(" ", testPosition.revealContent(GridType.TARGET_GRID));
     }
 
+    // Test revealContent after an attack method when Position is in HasNoBoatState.
     @Test
     void hasNoBoatRevealContentAfterAttack() {
         Position testPosition = new Position(Column.A, Row._0);
@@ -51,14 +53,54 @@ class PositionTest {
         assertEquals("o", testPosition.revealContent(GridType.TARGET_GRID));
     }
 
+    // Test the switch from HasNoBoatState to HasBoatState.
+    @Test
+    void stateChange() throws NoSuchFieldException, IllegalAccessException {
+        PatrolBoat aPatrolBoat = new PatrolBoat();
+        Position testPosition = new Position(Column.A, Row._0);
+
+        Field stateField = Position.class.getDeclaredField("state");
+        stateField.setAccessible(true);
+
+        // Call UUT
+        testPosition.placeBoat(aPatrolBoat);
+
+        // Make assertion
+        assertEquals(HasBoatState.class, stateField.get(testPosition).getClass());
+    }
+
     // Test attack method when Position is in HasBoatState.
     @Test
     void hasBoatAttack() {
+        PatrolBoat aPatrolBoat = new PatrolBoat();
+        Position testPosition = new Position(Column.A, Row._0);
+        testPosition.placeBoat(aPatrolBoat);
+
+        // Call UUT and make assertion.
+        assertFalse(testPosition.attack());
     }
 
-    // Test revealContent method when Position is in HasBoatState.
+    // Test revealContent method before an attack when Position is in HasBoatState.
     @Test
-    void hasBoatRevealContent() {
+    void hasBoatRevealContentBeforeAttack() {
+        PatrolBoat aPatrolBoat = new PatrolBoat();
+        Position testPosition = new Position(Column.A, Row._0);
+        testPosition.placeBoat(aPatrolBoat);
+
+        // Call UUT and make assertion.
+        assertEquals(" ", testPosition.revealContent(GridType.TARGET_GRID));
+    }
+
+    // Test revealContent method after an attack when Position is in HasBoatState.
+    @Test
+    void hasBoatRevealContentAfterAttack() {
+        PatrolBoat aPatrolBoat = new PatrolBoat();
+        Position testPosition = new Position(Column.A, Row._0);
+        testPosition.placeBoat(aPatrolBoat);
+        testPosition.attack();
+
+        // Call UUT and make assertion.
+        assertEquals("X", testPosition.revealContent(GridType.TARGET_GRID));
     }
 
 }
