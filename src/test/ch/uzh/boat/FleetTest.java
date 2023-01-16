d716e6835880aa469834415b534cae8d87f64fe6
Mely <95911782+Meleac@users.noreply.github.com>
2023-01-16 17:56:08 +0100

    HumanPlayerPlacementTest

--- src/test/ch/uzh/boat/FleetTest.java
+++ src/test/ch/uzh/boat/FleetTest.java
@@ -1,6 +1,11 @@
 package ch.uzh.boat;
 
+import ch.uzh.board.position.Position;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,18 +18,42 @@ class FleetTest {
         assertTrue(fleet.stillStanding());
     }
 
+
     //Test when only one boat is destroyed
     @Test
-    void oneShipDestroyed() {
+    void oneShipDestroyed() throws NoSuchFieldException, IllegalAccessException {
         Fleet fleet = new Fleet();
-        for (Boat boat: fleet) {
-
+        Iterator<Boat> it = fleet.iterator();
+        while (it.hasNext()) {
+            Boat boat = it.next();
+            if (boat instanceof Carrier) {
+                Field spanField = Boat.class.getDeclaredField("span");
+                spanField.setAccessible(true);
+                List<Position> span = (List<Position>) spanField.get(boat);
+                for (int i=0; i<boat.getSize(); i++){
+                    Position position = span.get(i);
+                    boat.takeHitAtPosition(position);
+                }
+                break;
+            }
         }
+        assertTrue(fleet.stillStanding());
     }
 
     @Test
-    void allShipsDestroyed() {
-
+    void allShipsDestroyed() throws NoSuchFieldException, IllegalAccessException {
+        Fleet fleet = new Fleet();
+        Iterator<Boat> it = fleet.iterator();
+        while (it.hasNext()) {
+            Boat boat = it.next();
+            Field spanField = Boat.class.getDeclaredField("span");
+            spanField.setAccessible(true);
+            List<Position> span = (List<Position>) spanField.get(boat);
+            for (int i = 0; i < boat.getSize(); i++) {
+                Position position = span.get(i);
+                boat.takeHitAtPosition(position);
+            }
+        }
+        assertFalse(fleet.stillStanding());
     }
-
 }
